2f236fbb3dad918b643d714cde96abb8de2b51fe
Felipe Ferreira Alves <felipealvesferreira05@gmail.com>
2024-11-14 16:56:38 -0300

    Melhoria no forms
    Estudei como melhorar o forms. Mudança aplicada na "Série"

--- Entreterimento/src/app/components/series/series.component.ts
+++ Entreterimento/src/app/components/series/series.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { CardComponent } from '../card/card.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { SeriesService } from '../../services/series.service';
 import { Series } from '../../interfaces/series';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-series',
   standalone: true,
-  imports: [NavbarComponent,FormsModule,CardComponent,MatInputModule,MatButtonModule],
+  imports: [NavbarComponent,FormsModule,CardComponent,MatInputModule,MatButtonModule,ReactiveFormsModule],
   templateUrl: './series.component.html',
   styleUrl: './series.component.css'
 })
 export class SeriesComponent implements OnInit {
-  nome: string = '';
+  formSerie = new FormGroup({
+    nome: new FormControl('')
+  });
+
   series: Series[] = [];
   constructor(private sService: SeriesService) {}
 
@@ -24,7 +29,7 @@ export class SeriesComponent implements OnInit {
   }
  
   addSerie(){
-    this.sService.getSerie(this.nome).subscribe((x) => {
+    this.sService.getSerie(this.formSerie.value.nome!).subscribe((x) => {
       if(x.Response != "False"){
         let adicionar: Series = {
           id: x.imdbID,
